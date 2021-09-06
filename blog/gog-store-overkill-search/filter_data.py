241581489dbb654ef24c2af0c0d7c46dd27caa18
glumia <glumia@users.noreply.github.com>
2021-09-06 06:24:09 +0000

    deploy: glumia/glumia-blog-files@1ff5da2355afbe1038aa60bb61889e432ceb4424

--- blog/gog-store-overkill-search/filter_data.py
+++ blog/gog-store-overkill-search/filter_data.py
@@ -5,10 +5,9 @@ import requests
 # Merge JSON data in a single list
 data = []
 for i in range(1, 13):
-    fp = open("games" + str(i) + ".json", "r")
-    tmp = json.load(fp)
-    data = data + tmp["products"]
-    fp.close()
+    with open("games" + str(i) + ".json", "r") as fp:
+        tmp = json.load(fp)
+        data = data + tmp["products"]
 
 # Filter - Price <= 1.30 and Price > 0
 data = [
@@ -17,14 +16,14 @@ data = [
     if float(x["price"]["finalAmount"]) <= 1.3 and float(x["price"]["finalAmount"]) > 0
 ]
 
-# Filter - Remove DLC's
+# Filter - Remove DLCs
 gog_url = "https://www.gog.com"
 DLC_flag_text = "To play this game you also need"
 data = [x for x in data if DLC_flag_text not in requests.get(gog_url + x["url"]).text]
 
+if len(data) == 0:
+    print("No games in the [0.01, 1.30] price range!")
+    exit
 
-if len(data) > 0:
-    for el in data:
-        print(el["title"] + "\tPrice: " + el["price"]["finalAmount"])
-else:
-    print("No games in the 0,01-1.30 price range!")
+for el in data:
+    print(el["title"] + "\tPrice: " + el["price"]["finalAmount"])
